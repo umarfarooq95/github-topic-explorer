9dffafd484776bb8d66a1054e8cac8b286017e2a
umarfarooq95 <mohamedumar931@gmail.com>
2021-01-06 20:51:16 +0530

    Implemented UI Part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+
+<div className="row">
+<div className="col-12">
+<input class="form-control" type="text" placeholder="Search"/>
+  </div>
+  </div>
+
+  <div className="row mt-4">
+<div className="col-12">
+  <p class="h3">Topics</p>
+  </div>
+  </div>
+
+<ul className="list-group list-group-flush">
+  <li className="list-group-item d-flex justify-content-between align-items-center">
+    <div className="row">
+    <div className="col-2">
+    <img className="img-thumbnail" width="200" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt=""/>
+      </div>
+      <div className="col-10">
+      <p class="h4">Cras justo odio
+      <span className="badge bg-primary rounded-pill float-right">14</span>
+      </p>
+    <p class="lead">
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+</p>
+      </div>
+    </div>
+  </li>
+
+  <li className="list-group-item d-flex justify-content-between align-items-center">
+    <div className="row">
+    <div className="col-2">
+    <img className="img-thumbnail" width="200" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt=""/>
+      </div>
+      <div className="col-10">
+      <p class="h4">Cras justo odio
+      <span className="badge bg-primary rounded-pill float-right">14</span>
+      </p>
+    <p class="lead">
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+</p>
+      </div>
+    </div>
+  </li>
+
+  <li className="list-group-item d-flex justify-content-between align-items-center">
+    <div className="row">
+    <div className="col-2">
+    <img className="img-thumbnail" width="200" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt=""/>
+      </div>
+      <div className="col-10">
+      <p class="h4">Cras justo odio
+      <span className="badge bg-primary rounded-pill float-right">14</span>
+      </p>
+    <p class="lead">
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+</p>
+      </div>
+    </div>
+  </li>
+
+
+
+
+</ul>
+
     </div>
   );
 }
